19351995bbaf106d0dc4b5c587ed43e6a6eb4471
Justin C Presley <justincpresley@gmail.com>
2021-11-10 10:28:30 -0600

    example of some scripts

---
+++ cat_issues/limit.sh
@@ -0,0 +1,3 @@
+curl -u user:token -H "Accept: application/vnd.github.v3+json" https://api.github.com/rate_limit
+
+#curl -u user:token -H "Accept: application/vnd.github.v3+json" https://api.github.com/users/user
