8dedfb7c4f0fc9076ff5af7d25057bf789ed7b7d
Justin C Presley <justincpresley@gmail.com>
2021-11-12 08:25:44 -0600

    changes to script

--- cat_issues/pull.py
+++ cat_issues/pull.py
@@ -12,10 +12,12 @@ def read_issues_from_file(file):
 def write_issues_to_file(data, file):
     with open(file, 'wb') as filehandle:
         pickle.dump(data, filehandle)
-def check_issues(issues):
+def check_issues(issues, skipped_issues):
     num = 1
     for i in issues:
-        if ("url" in i.keys()) and int(i["number"]) == num:
+        while num in skipped_issues:
+            num = num + 1
+        if ("url" in i.keys()):
             num = num + 1
             continue
         else:
@@ -38,10 +40,12 @@ def request_issue(repository, token, issue_number, tempfile):
     os.remove(tempfile)
     return result
 
-def request_all_issues(repository, token, issues, tempfile):
-    number = len(issues) + 1
+def request_all_issues(repository, token, issues, tempfile, skipped_issues):
+    number = int(issues[-1]["number"]) + 1
     run = True
     while run:
+        while number in skipped_issues:
+            number = number + 1
         data = request_issue(repository, token, number, tempfile)
         if "url" not in data.keys():
             print(data)
@@ -54,20 +58,21 @@ def main():
     repository = "https://api.github.com/repos/ansible/ansible"
     issues_file = "./issues.data"
     temp_issue_file = "./issue"
-    token = read_git_token("/home/corbin/.gittoken2")
+    skipped_issues = [9891]
+    token = read_git_token("/home/corbin/.gittoken")
     issues = read_issues_from_file(issues_file)
     print(f"# Github Key: {token}")
-    status = check_issues(issues)
+    status = check_issues(issues, skipped_issues)
     print(f"# Issues: {len(issues)}, Status: {status}\n")
 
     print(f"Starting to Download More Issues...")
     try:
-        request_all_issues(repository, token, issues, temp_issue_file)
+        request_all_issues(repository, token, issues, temp_issue_file, skipped_issues)
     except:
         clean_up_before_exit(issues, temp_issue_file)
     finally:
         print(f"Done with Downloading.")
-        status = check_issues(issues)
+        status = check_issues(issues, skipped_issues)
         print(f"\n# Issues: {len(issues)}, Status: {status}")
         write_issues_to_file(issues, issues_file)
 
